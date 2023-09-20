7f64649aef631870362c8d25b34e0e207dfc5c17
eggmun98 <eggmun98@gmail.com>
2023-09-20 22:03:33 +0900

    [2023.09.20] #3.2 GET Requests part Two 강의 수강

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("get 메소드 잘 작동");
+
+app.get("/", handleHome);
+
 const handleListening = () => console.log("시작sssssss");
 
 app.listen(PORT, handleListening);
