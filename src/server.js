22957599c4cdc6e376fdf6a45b8b38758d71987a
eggmun98 <eggmun98@gmail.com>
2023-09-20 22:16:27 +0900

    [2023.09.20] #3.3 Responses 강의 수강

--- src/server.js
+++ src/server.js
@@ -4,9 +4,19 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("get 메소드 잘 작동");
+// req와 res는 express한테 받는다 req와 res는 오브젝트인다
+// 즉 req{}, res{}  이런 형태로 req와 res를 넣어줄꺼다
+const handleHome = (req, res) => {
+  return res.send("안녕");
+};
+
+const handleLogin = (req, res) => {
+  return res.end();
+};
 
 app.get("/", handleHome);
+
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log("시작sssssss");
 
