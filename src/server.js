16defc86439cec90806a9a1785d8795db079da4c
eggmun98 <eggmun98@gmail.com>
2023-09-21 21:21:15 +0900

    [2023.09.21] #3.6 Middlewares part Two 강의 수강

--- src/server.js
+++ src/server.js
@@ -10,8 +10,17 @@ const handleHome = (req, res) => {
   return res.send("<h1>hello</h1>");
 };
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`s누군가 req.url로 가려고 한다. ${req.url}`);
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} : ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("허가함 다음으로 이동하거라");
   next();
 };
 
@@ -21,9 +30,16 @@ const handleLogin = (req, res) => {
   return res.end();
 };
 
-app.get("/", handleHome);
+const handleProtected = (req, res) => {
+  return res.send("웰컴~~");
+};
 
-app.get("/login", gossipMiddleware, handleLogin);
+app.use(loggerMiddleware);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("protected", handleProtected);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log("시작sssssss");
 
