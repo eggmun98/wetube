715fdb34e358f7fb4c6fdf732c88fcc9a02e7afb
eggmun98 <eggmun98@gmail.com>
2023-09-24 18:38:46 +0900

    [2023.09.24] #4.7 URL Parameters part One 강의 수강

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,10 @@ import { deleteVideo, edit, see, upload } from "./controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
+videoRouter.get("/upload", upload); // /upload를 /:id보다 위에서 적은 이유는
+// /upload를 /:id보다 아래에 적는다면 express의 req가 "/:id" 이 위치가 아이디 자리인줄 알고
+// /upload를 보면 거기도 id 자리인줄 알기 때문이다.
+// 그래서 /upload를 제일 위에 적고 아래는 파라미터URL을 적는다.
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
