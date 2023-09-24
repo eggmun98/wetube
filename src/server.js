5bbe7a08aea65bb66664118ab1eeccab3b2c3e7d
eggmun98 <eggmun98@gmail.com>
2023-09-24 15:07:16 +0900

    [2023.09.24] #3.11 External Middlewares 강의 수강

--- src/server.js
+++ src/server.js
@@ -1,45 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const loggerMiddleware = morgan("dev"); // 모간은 미들웨어이다.
 
-// req와 res는 express한테 받는다 req와 res는 오브젝트인다
-// 즉 req:{...}, res:{...}  이런 형태로 req와 res를 넣어줄꺼다
-const handleHome = (req, res) => {
-  return res.send("<h1>hello</h1>");
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("heelog");
 };
 
-const loggerMiddleware = (req, res, next) => {
-  console.log(`${req.method} : ${req.url}`);
-  next();
+const login = (req, res) => {
+  return res.send("login");
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("허가함 다음으로 이동하거라");
-  next();
-};
-
-// 브라우저가 요청을 하면 서버는 무조건 응답을 해줘야 한다.
-// html파일, 객체, 배열 등등 많은 것을 응답해 줄수 있다.
-const handleLogin = (req, res) => {
-  return res.end();
-};
-
-const handleProtected = (req, res) => {
-  return res.send("웰컴~~");
-};
-
-app.use(loggerMiddleware);
-app.use(privateMiddleware);
-
-app.get("/", handleHome);
-app.get("protected", handleProtected);
-app.get("/login", handleLogin);
+app.use(loggerMiddleware); // app.use부터 코드를 적어야 한다. 만약 app.use가 19번줄이고 app.get("/". home)가 18번줄이면 미들웨어는 20번 코드에서만 작동
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => console.log("시작sssssss");
 
