efc78380dd3e72f93bb21eb7374a3a17593898d3
eggmun98 <eggmun98@gmail.com>
2023-09-26 21:32:06 +0900

    [2023.09.26] #5.2 Partials 강의 수강

--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -1,11 +1,12 @@
 export const trending = (req, res) => {
-  return res.render("home");
+  res.render("home");
 };
 export const see = (req, res) => {
-  console.log(req.params);
-  return res.send("Watch");
+  res.render("watch");
 };
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => {
+  res.render("edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
