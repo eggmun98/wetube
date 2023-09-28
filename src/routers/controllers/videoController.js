22d64f42916ba005c0cbdff5cb5d703218b5e9c7
eggmun98 <eggmun98@gmail.com>
2023-09-28 21:38:58 +0900

    [2023.09.28] #6.6 More Practice part Two 강의 수강

--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -51,6 +51,17 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
+  console.log(req.body);
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 1,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
   // here we will add a video to the videos array.
   return res.redirect("/");
 };
