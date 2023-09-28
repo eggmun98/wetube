c3c5672e265ac213f7c4d36ec05aa4c1b7836b97
eggmun98 <eggmun98@gmail.com>
2023-09-28 18:25:26 +0900

    [2023.09.28] #6.1 Array Database part Two 강의 수강

--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -28,11 +28,11 @@ let videos = [
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   console.log("Show video", id);
   const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watch ${video.title}` });
+  res.render("watch", { pageTitle: `Watch ${video.title}`, video });
 };
 export const edit = (req, res) => {
   res.render("edit", { pageTitle: "Edit Video" });
