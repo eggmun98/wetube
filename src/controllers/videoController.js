f3ccd5de91b96ac0f3b724665795a27e4afa9cbf
eggmun98 <eggmun98@gmail.com>
2023-09-29 15:53:57 +0900

    [2023.09.29] #6.16 Creating a Video part Two 강의 수강

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find();
+  console.log(videos);
   res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -25,7 +26,7 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   console.log(req.body);
   const { title, description, hashtags } = req.body;
   const video = new Video({
@@ -38,6 +39,6 @@ export const postUpload = (req, res) => {
       rating: 0,
     },
   });
-  console.log(video);
+  const dbVideo = await video.save();
   return res.redirect("/");
 };
