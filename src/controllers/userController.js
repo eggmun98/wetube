59f69fbb60c72063252ec1eec574ba6535bcf6ae
eggmun98 <eggmun98@gmail.com>
2023-10-02 13:33:14 +0900

    [2023.10.02] #7.5 Login part One 강의 수강

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,18 +19,39 @@ export const postJoin = async (req, res) => {
     });
   }
 
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error_message,
+    });
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with this username docs not exists",
+    });
+  }
+  res.end();
 };
 
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("delete user");
-export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See");
