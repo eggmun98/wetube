03b0f8b3a962c4b85743942b3a3703bba60c2cec
eggmun98 <eggmun98@gmail.com>
2023-10-02 14:04:25 +0900

    [2023.10.02] #7.7 Sessions and Cookies part One 강의 수강

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import session from "express-session";
 
 // console.log( process.cwd() )  현재 작업 위치의 디렉토리
 // 현재 작업 디렉토리는 노드를 시작하는 디렉토리이다.
@@ -14,6 +15,14 @@ app.set("view engine", "pug"); // 뷰 엔진을 pug로 쓴다고 등록
 app.set("views", process.cwd() + "/src/views"); // 뷰 엔진의 디렉토리 위치 변경
 app.use(logger); // app.use부터 코드를 적어야 한다. 만약 app.use가 19번줄이고 app.get("/". home)가 18번줄이면 미들웨어는 20번 코드에서만 작동
 app.use(express.urlencoded({ extended: true })); // express가 form data를 읽을 수 있게 해줌 즉 자바스크립트 오브젝트 형식으로 바꿔줌
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
