d852a289083818e798c99e186f152a9c194f7e68
eggmun98 <eggmun98@gmail.com>
2023-10-03 19:31:44 +0900

    [2023.10.03] Comment 로그인 관련 코드 설명 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -40,42 +40,50 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
+// 일반 로그인
 export const postLogin = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body; // 유저가 입력한 값을 가져옴
   const pageTitle = "Login";
-  const user = await User.findOne({ username, socialOnly: false });
+  const user = await User.findOne({ username, socialOnly: false }); // 여기서 소셜온리를 페일한 이유는 github로 로그인 했는지 안했는지 구분을 위해 작성
+  // db에서 유저가 입력한 username에 맞는 username의 유저 데이터를 가져옴
+
   //   const exists = await User.exists({ username });
   if (!user) {
+    // 만약 유저가 잘못된 유저네임을 입력했다면 id가 틀렸다는 뜻이므로 잘못된 유저네임이라고 출력
     return res.status(400).render("login", {
       pageTitle,
       errorMessage: "An account with this username docs not exists",
     });
   }
-  const ok = await bcrypt.compare(password, user.password);
+  const ok = await bcrypt.compare(password, user.password); // password를 해쉬화 해서 db에 있는 패스워드랑 비교
   if (!ok) {
+    // ok가 없다는 건 비밀번호가 틀렸다는 뜻이므로 로그인 하라고 출력
     return res.status(400).render("login", {
       pageTitle,
       errorMessage: "Wrong password",
     });
   }
-  req.session.loggedIn = true;
+  req.session.loggedIn = true; // 로그인에 성공했으니 세션에 로그인 했다고 등록
   req.session.user = user;
   console.log(req.session);
   return res.redirect("/");
 };
 
+// 인증코드를 받기 위해 작성
 export const startGithubLogin = (req, res) => {
   const baseUrl = `https://github.com/login/oauth/authorize`;
   const config = {
     client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
-  };
-  const params = new URLSearchParams(config).toString();
-  const finalUrl = `${baseUrl}?${params}`;
-  return res.redirect(finalUrl);
+  }; // github oauth 설정 url이 길어지니 두개로 baseUrl와 config로 나눔
+  const params = new URLSearchParams(config).toString(); // URL 쿼리 문자열로 변환을 위해 작성
+  const finalUrl = `${baseUrl}?${params}`; // 두개 합쳐서 리다이렉트하기 위해 작성
+  return res.redirect(finalUrl); // 리다이렉트로 유저가 허용하면 인증코드 발급
 };
 
+// 인증코드 발급 후 이 코드 실행
+// 토큰을 발급 받기 위해 작성(인증코드가 필요함)
 export const finishGithubLogin = async (req, res) => {
   const baseUrl = `https://github.com/login/oauth/access_token`;
   const config = {
@@ -89,11 +97,12 @@ export const finishGithubLogin = async (req, res) => {
     await fetch(finalUrl, {
       method: "POST",
       headers: {
-        Accept: "application/json",
+        Accept: "application/json", // api 요청할 때 보내는 방식도 적어줘야 작동함
       },
     })
   ).json();
 
+  // tokenRequest에서 액세스 토큰이 있는지 확인 후 있으면 유저 데이터를 가져옴(단 여기서는 유저 이메일은 못가져옴)
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
     const apiUrl = "https://api.github.com";
@@ -114,17 +123,20 @@ export const finishGithubLogin = async (req, res) => {
       })
     ).json();
 
+    // 로그인한 유저의 이메일 정보만 emailObj에 담음
     const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
 
+    // emailObj가 없으면 로그인을 안했다는거니 로그인페이지로 리다이렉트함
     if (!emailObj) {
       return res.redirect("/login");
     }
 
     // github 로그인
-    let user = await User.findOne({ email: emailObj.email });
+    let user = await User.findOne({ email: emailObj.email }); // db에  git이메일로 만들어진 계정이 있는지 확인
     if (!user) {
+      // user가 없다면 github로 만들어진 계정이 없다는 뜻이므로 db에 유저 계정을 추가 즉 회원가입
       user = await User.create({
         avatarUrl: userData.avatar_url,
         name: userData.name,
@@ -135,7 +147,7 @@ export const finishGithubLogin = async (req, res) => {
         location: userData.location,
       });
     }
-    req.session.loggedIn = true;
+    req.session.loggedIn = true; // 세션에 유저가 로그인 했다고 저장
     req.session.user = user;
     return res.redirect("/");
   } else {
@@ -144,7 +156,7 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  req.session.destroy();
+  req.session.destroy(); // 세션 데이터를 삭제 즉 로그아웃
   return res.redirect("/");
 };
 
