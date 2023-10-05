47dfca19d388e9fc5e702d01c561421acb4bd852
eggmun98 <eggmun98@gmail.com>
2023-10-05 22:23:51 +0900

    [2023.10.05] #8.7 File Uploads part Two 강의 수강

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -167,7 +167,7 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
@@ -179,6 +179,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
