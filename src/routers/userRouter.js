de7a4e6139744e925b73428dc8cff06eee8b943a
eggmun98 <eggmun98@gmail.com>
2023-10-05 21:19:16 +0900

    [2023.10.05] #8.4 Change Password part One 강의 수강

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,6 +8,8 @@ import {
   finishGithubLogin,
   getEdit,
   postEdit,
+  getChangePassword,
+  postChangePassword,
 } from "../controllers/userController";
 import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
@@ -17,6 +19,10 @@ userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit); // all 함수를 쓰면 미들웨어를 get이나 post에 둘다 사용가능
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+userRouter
+  .route("/change-password")
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.get("/:id", see);
 
 export default userRouter;
