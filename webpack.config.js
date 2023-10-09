a7e1bb1770bb8afac9b363fdbf78c7b553e6b741
eggmun98 <eggmun98@gmail.com>
2023-10-09 16:34:09 +0900

    [2023.10.09] #9.4 SCSS loader 강의 수강

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,12 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"], // 로더를 역순으로 작성해야 함 => 웹팩은 뒤에서부터 시작하기
+        // scss파일을 css로 변환시키고 그 코드들을 css-loader에 전달해 css를 js로 변환 시킨다
+        // 마지막으로 style-loader가 js로 변환된 css를 dom에 전달 즉 브라우저에게 보이게 함
+      },
     ],
   },
 };
